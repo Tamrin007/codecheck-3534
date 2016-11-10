c2fb06751fd27dca11e515a6605f712f3f8cdb99
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-11-10 22:10:47 +0900

    コメントを追加、コードを微修正

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 }
 
 func run(args []string) {
+	// 各入力を数値に変換
 	daysInYear, err := strconv.Atoi(args[0])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
@@ -30,6 +31,8 @@ func run(args []string) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 	}
+
+	// 対象の日付をパースして数値に変換
 	input := strings.Split(args[3], "-")
 	var target date
 	target.year, err = strconv.Atoi(input[0])
@@ -49,11 +52,13 @@ func run(args []string) {
 	stockDaysPerYear := daysInYear % daysInMonth
 	// その年を終えた時のストック日数
 	stockDays := stockDaysPerYear * target.year % daysInMonth
-	// 閏月の回数
+	// その年までの閏月の回数
 	leapMonth := stockDaysPerYear * (target.year - 1) / daysInMonth
 	// 月の上限
 	limitMonth := daysInYear / daysInMonth
 	// 閏年判定
+	// その年を終えた時点で「ストック日数」が「1年でストックされる日数」を
+	// 下回っていたら月が追加されている
 	if stockDays < stockDaysPerYear {
 		limitMonth++
 	}
@@ -63,21 +68,27 @@ func run(args []string) {
 		fmt.Println("-1")
 		return
 	}
+
 	// 無効な月をチェック
 	if target.month > limitMonth {
 		fmt.Println("-1")
 		return
 	}
 
-	pastDaysInYears := (daysInYear - stockDaysPerYear) * (target.year - 1)
-	pastDaysInMonths := daysInMonth * (target.month - 1 + leapMonth)
+	// 対象の日付までの日数
+	pastDays :=
+		(daysInYear-stockDaysPerYear)*(target.year-1) +
+			daysInMonth*(target.month-1+leapMonth) +
+			target.day
 
-	pastDays := pastDaysInYears + pastDaysInMonths + target.day
+	// 曜日を算出
 	dayOfWeek := pastDays % daysInWeek
 
+	// AはASCIIコード65なので、64に曜日の剰余を足す、割り切れる場合は1週間の日数を足す
+	// 1 -> A曜日、2 -> B曜日、、、0 -> 最後の曜日
 	if dayOfWeek != 0 {
-		fmt.Println(string(65 + dayOfWeek - 1))
+		fmt.Println(string(64 + dayOfWeek))
 		return
 	}
-	fmt.Println(string(65 + dayOfWeek + daysInWeek - 1))
+	fmt.Println(string(64 + daysInWeek))
 }
